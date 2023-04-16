11cabd0f9f1e4387ec28ea04219815f978cc0836
bluedo999 <83590521+bluedo999@users.noreply.github.com>
2023-04-16 16:24:50 +0900

    Add files via upload

--- bitcoinAutoTradeWithAI.py
+++ bitcoinAutoTradeWithAI.py
@@ -1,15 +1,20 @@
 #predict_price를 업비트 KRW 마켓에 있는 품목 전체를 목록으로 선정하여 분석하고 매매시작
+# fbprophet 모듈을 설치
+# 해당 코드에서는 from fbprophet import Prophet 에러가 발생합니다. fbprophet 모듈을 설치해야 합니다. 그리고 upbit 변수를 선언하지 않아서 에러가 발생합니다. upbit = pyupbit.Upbit(access, secret) 코드를 추가해야 합니다.
 
 import time
 import pyupbit
 import datetime
 import schedule
 from fbprophet import Prophet
+!pip install fbprophet
 
-access = "LecqE9QTwyW2Ia6OlppLK75heiIb0hDAsYxLRnwS"
-secret = "vWnj7tX45Gh9V6BjVCPhpejD2NBYCIqM6dxrj03C"
+access = "RtNkYNkqltxtJbE9E9OfI2se4xlWDxxKRx5mrQhO"
+secret = "FkougvxZA0nnVet6DdNNktj9vXSjpVnDixfe6sxz"
+
 
 tickers = pyupbit.get_tickers(fiat="KRW")
+
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
@@ -17,14 +22,17 @@ def get_target_price(ticker, k):
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
+
 def get_start_time(ticker):
     """시작 시간 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
     start_time = df.index[0]
     return start_time
 
+
 def get_balance(ticker):
     """잔고 조회"""
+    upbit = pyupbit.Upbit(access, secret)
     balances = upbit.get_balances()
     for b in balances:
         if b['currency'] == ticker:
@@ -34,9 +42,11 @@ def get_balance(ticker):
                 return 0
     return 0
 
+
 def get_current_price(ticker):
     """현재가 조회"""
     return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
+
 
 predicted_close_price = 0
 def predict_price(ticker):
@@ -57,6 +67,7 @@ def predict_price(ticker):
     closeValue = closeDf['yhat'].values[0]
     predicted_close_price = closeValue
 
+
 predict_price(tickers)
 schedule.every().hour.do(lambda: predict_price(tickers))
 
@@ -75,15 +86,15 @@ while True:
         if start_time < now < end_time - datetime.timedelta(seconds=10):
             target_price = get_target_price(tickers, 0.5)
             current_price = get_current_price(tickers)
-            if target_price < current_price and current_price < predicted_close_price:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order(tickers, krw*0.9995)
-        else:
-            btc = get_balance(tickers)
+        if target_price < current_price and current_price < predicted_close_price:
+            krw = get_balance("KRW")
+            if krw > 5000:
+                upbit.buy_market_order(tickers, krw*0.9995)
+            else:
+                btc = get_balance(tickers)
             if btc > 0.00008:
                 upbit.sell_market_order(tickers, btc*0.9995)
-        time.sleep(1)
+            time.sleep(1)
     except Exception as e:
-        print(e)
-        time.sleep(1)
+            print(e)
+            time.sleep(1)
